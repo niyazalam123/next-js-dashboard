28c34b201629eaa81da7b0e3440e3d0c036d5965
niyazalam123 <alamniyaz173@gmail.com>
2024-07-06 00:42:52 +0530

    error fixed

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,6 +4,7 @@ export async function GET(){
     try {
         const response = NextResponse.json("Logout Successfully",{status:200});
         response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})
+        return NextResponse.json("Logout successfully",{status:500});
     } catch (error) {
         return NextResponse.json("internal server error",{status:500});
     }
