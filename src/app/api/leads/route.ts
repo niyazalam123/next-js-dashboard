223d21755376d7de29206339a353d4d3d4b6df1d
niyazalam123 <alamniyaz173@gmail.com>
2024-07-06 00:39:46 +0530

    error solved

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,38 +1,34 @@
-// import dbConnect from "@/app/dbconnects/dbConnects";
-// import LeadModel from "@/app/models/Lead";
-// import { NextResponse, NextRequest } from "next/server";
+import dbConnect from "@/app/dbconnects/dbConnects";
+import LeadModel from "@/app/models/Lead";
+import { NextResponse, NextRequest } from "next/server";
 
-// // secure api route
-// function checkApiKey(req: Request) {
-//     const apiKey = req.headers.get('x-api-key');
-//     if (!apiKey || apiKey !== process.env.NEXT_PUBLIC_lEADS_API_KEY) {
-//         return false;
-//     }
-//     return true;
-// }
+// secure api route
+function checkApiKey(req: Request) {
+    const apiKey = req.headers.get('x-api-key');
+    if (!apiKey || apiKey !== process.env.NEXT_PUBLIC_lEADS_API_KEY) {
+        return false;
+    }
+    return true;
+}
 
-// export async function POST(request: NextRequest) {
-//     // check varifications of api key
-//     if(!checkApiKey(request)){
-//         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-//     }
+export async function POST(request: NextRequest) {
+    // check varifications of api key
+    if(!checkApiKey(request)){
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
 
-//     try {
-//         dbConnect();
-//         const payLoad = await request.json();
-//         const leads = new LeadModel({
-//             ...payLoad
-//         });
-//         await leads.save();
-//         const response = NextResponse.json({message:"data upload successfully",success: true},{status:200});
-//         response.headers.set('Access-Control-Allow-Origin', '*');
-//         response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-//         response.headers.set('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
-//         return response;
-//     } catch (error:any) {
-//         return NextResponse.json({ message: "Error uploading data", success: false, error: error.message }, { status: 500 });
-//     }
-// }
+    try {
+        dbConnect();
+        const payLoad = await request.json();
+        const leads = new LeadModel({
+            ...payLoad
+        });
+        await leads.save();
+        return NextResponse.json({message:"data upload successfully",success: true},{status:200});;
+    } catch (error:any) {
+        return NextResponse.json({ message: "Error uploading data", success: false, error: error.message }, { status: 500 });
+    }
+}
 
 
 // // create get route
